42a83eb1ca4b2d0ab1bb80542c18d4c849ea50b8
Виталий Кулешин <antivitla@gmail.com>
2015-02-04 02:42:51 +0300

    Сохранение состояний в localStorage

--- testwood.js
+++ testwood.js
@@ -1,48 +1,85 @@
-"use strict";
-/*globals $, getComputedStyle */
-
 
 function testwood(initialModules) {
-	if(document.getElementById("#testwood-menu")) return false;
-
-	var modules = initialModules ? initialModules : {};
-
-	var menu = document.createElement("div");
-	menu.id = "testwood-menu";
+	"use strict";
+	/*globals console, getComputedStyle, document, localStorage */
 	
-
-	// в меню добаляем элементы
-	for(var i in modules) {
-		var a = document.createElement("a");
-		a.setAttribute("data-testwood-toggle", i);
-		a.innerHTML = modules[i];
-		a.addEventListener("click", function() {
-			document.body.classList.toggle("testwood-"+this.getAttribute("data-testwood-toggle"));
-			this.classList.toggle("active");
-		});
-		menu.appendChild(a);
+	if (document.getElementById("testwood-menu")) {
+		console.warn("Testwood уже инициализирован");
+		return;
 	}
 
+	var modules = {},
+		menu = document.createElement("div"),
+		iterator,
+		button,
+		allElements;
+		
+	function readState(moduleName) {
+		return (localStorage.getItem("testwood-" + moduleName) === "true" ? true : false);
+	}
+	
+	function sync2localstorage(moduleObj) {
+		localStorage.setItem("testwood-" + moduleObj.name, moduleObj.active);
+	}
+	
+	function sync2dom(moduleObj) {
+		if (moduleObj.active) {
+			moduleObj.element.classList.add("active");
+			document.body.classList.add("testwood-" + moduleObj.name);
+		} else {
+			moduleObj.element.classList.remove("active");
+			document.body.classList.remove("testwood-" + moduleObj.name);
+		}
+	}
+	
+	function handleClick(event) {
+		var moduleName = event.target.getAttribute("data-testwood-toggle");
+		// переключаем состояние в модели
+		modules[moduleName].active = !modules[moduleName].active;
+		// синхронизируем DOM и localstorage
+		sync2dom(modules[moduleName]);
+		sync2localstorage(modules[moduleName]);
+	}
+
+	
+	// в меню добавляем элементы
+	for (iterator in initialModules) {
+		if (initialModules.hasOwnProperty(iterator)) {
+			// создаем кнопку
+			button = document.createElement("a");
+			button.setAttribute("data-testwood-toggle", iterator);
+			button.innerHTML = initialModules[iterator];
+			// навешиваем переключение
+			button.addEventListener("click", handleClick);
+			// регистрируем модуль (и грузим предварительное состояние)
+			modules[iterator] = {
+				active: readState(iterator),
+				label: initialModules[iterator],
+				name: iterator,
+				element: button
+			};
+			sync2dom(modules[iterator]);
+			// добавляем в меню
+			menu.appendChild(button);
+		}
+	}
+
+	menu.id = "testwood-menu";
 	document.body.appendChild(menu);
 
 	// выделяем flex
-	var allElements = Array.prototype.slice.call(document.body.getElementsByTagName("*"), 0);
+	allElements = Array.prototype.slice.call(document.body.getElementsByTagName("*"), 0);
 	allElements.forEach(function (tag) {
 		if (getComputedStyle(tag).display === "flex") {
 			tag.setAttribute("data-test-flex", "");
 		}
 	});
-
-	return {
-		addModule: function(title, label) {
-			modules[title] = label;
-		}
-	}
 }
 
 
 // отложенная инициализация
 document.addEventListener("DOMContentLoaded", function () {
+	"use strict";
 	var api = testwood({
 		line: "Высота строки",
 		flex: "Flex",
@@ -50,5 +87,4 @@ document.addEventListener("DOMContentLoaded", function () {
 		title: "Названия",
 		space: "Разрядить"
 	});
-});	
-
+});
