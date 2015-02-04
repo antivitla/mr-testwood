3dcdf2f493110cbfeafffc02ac513ca9dc5b3bed
Виталий Кулешин <antivitla@gmail.com>
2015-02-04 03:54:34 +0300

    Берем названия из комментариев также

--- testwood.js
+++ testwood.js
@@ -7,6 +7,8 @@ function testwood(initialModules) {
 		console.warn("Testwood уже инициализирован");
 		return;
 	}
+	
+	/* Хранение модулей (параметров) тестирования в едином месте */
 
 	var modules = {},
 		menu = document.createElement("div"),
@@ -31,6 +33,12 @@ function testwood(initialModules) {
 			document.body.classList.remove("testwood-" + moduleObj.name);
 		}
 	}
+	
+	
+	
+	
+	
+	/* По клику на кнопках меню вкл/выкл параметры какие-то */
 	
 	function handleClick(event) {
 		var moduleName = event.target.getAttribute("data-testwood-toggle");
@@ -68,11 +76,56 @@ function testwood(initialModules) {
 	document.body.appendChild(menu);
 
 	// выделяем flex
+	
 	allElements = Array.prototype.slice.call(document.body.getElementsByTagName("*"), 0);
 	allElements.forEach(function (tag) {
 		if (getComputedStyle(tag).display === "flex") {
 			tag.setAttribute("data-test-flex", "");
 		}
+	});
+	
+	
+	// так же нужно уметь пропарсить тесты и взять например комменты все
+	// и пихнуть их например перед блоком, а потом удалять опять
+	
+	function parseTestBlocks(callback, callbackOfCallback) {
+		var allTestBlocks = document.querySelectorAll("*[data-test]"),
+			i;
+		for (i = 0; i < allTestBlocks.length; i = i + 1) {
+			callbackOfCallback(callback(allTestBlocks[i]));
+		}
+	}
+	
+	function findComment(element) {
+		var iterator = element.previousSibling,
+			comment;
+		
+		while (iterator) {
+			if (iterator.nodeType === 8) {
+				comment = iterator.nodeValue.trim();
+				break;
+			} else if (iterator.nodeType === 1) {
+				break;
+			} else {
+				iterator = iterator.previousSibling;
+			}
+		}
+		
+		return {
+			element: element,
+			comment: comment
+		};
+	}
+
+	parseTestBlocks(findComment, function (result) {
+		if (!result.comment) {
+			console.warn("Блок", result.element, "без комментариев");
+			return;
+		}
+		var title = document.createElement("header");
+		title.classList.add("testwood-comment");
+		title.innerHTML = result.comment;
+		result.element.parentElement.insertBefore(title, result.element);
 	});
 }
 
@@ -84,7 +137,10 @@ document.addEventListener("DOMContentLoaded", function () {
 		line: "Высота строки",
 		flex: "Flex",
 		bounds: "Границы",
+		space: "Разрядить",
 		title: "Названия",
-		space: "Разрядить"
+		comments: "Комментарии"
 	});
 });
+
+
